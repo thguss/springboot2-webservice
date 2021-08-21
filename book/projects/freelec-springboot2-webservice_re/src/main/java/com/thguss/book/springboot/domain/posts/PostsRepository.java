4f54c393b62a237c42624cb5ee1d7fad70547332
Sohyeon <thgus345@naver.com>
2021-08-21 20:40:03 +0900

    조회 화면 만들기

--- book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/domain/posts/PostsRepository.java
+++ book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package com.thguss.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> { // <Entity 클래스, PK 타입>
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/service/posts/PostsService.java
+++ book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.thguss.book.springboot.service.posts;
 
 import com.thguss.book.springboot.domain.posts.Posts;
 import com.thguss.book.springboot.domain.posts.PostsRepository;
+import com.thguss.book.springboot.web.dto.PostsListResponseDto;
 import com.thguss.book.springboot.web.dto.PostsResponseDto;
 import com.thguss.book.springboot.web.dto.PostsSaveRequestDto;
 import com.thguss.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
