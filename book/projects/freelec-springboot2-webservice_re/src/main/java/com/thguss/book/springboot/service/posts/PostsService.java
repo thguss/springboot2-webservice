ba8cfa143e97fc109f4305ec8fd77302b4e4a825
Sohyeon <thgus345@naver.com>
2021-08-21 16:30:31 +0900

    수정 / 조회 기능 추가 및 테스트

--- book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/service/posts/PostsService.java
+++ book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.thguss.book.springboot.service.posts;
 
+import com.thguss.book.springboot.domain.posts.Posts;
 import com.thguss.book.springboot.domain.posts.PostsRepository;
+import com.thguss.book.springboot.web.dto.PostsResponseDto;
 import com.thguss.book.springboot.web.dto.PostsSaveRequestDto;
+import com.thguss.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/web/PostsApiController.java
+++ book/projects/freelec-springboot2-webservice_re/src/main/java/com/thguss/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.thguss.book.springboot.web;
 
 import com.thguss.book.springboot.service.posts.PostsService;
+import com.thguss.book.springboot.web.dto.PostsResponseDto;
 import com.thguss.book.springboot.web.dto.PostsSaveRequestDto;
+import com.thguss.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,7 +14,17 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) { // 등록
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) { // 수정
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){ // 조회
+        return postsService.findById(id);
     }
 }
